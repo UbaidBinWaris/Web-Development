79feead3fc224f88ee85c24054636c96c102f7ee
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-08-24 13:01:11 +0500

    resume bana li ha
    completed

---
+++ cv/script.js
@@ -0,0 +1,12 @@
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
+
+    var element = document.querySelector(.resume); // The element you want to convert to PDF
+    html2pdf()
+        .from(element)
+        .set({
+            margin: 1,
+            filename: 'myCV.pdf',
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        })
+        .save(); */}
