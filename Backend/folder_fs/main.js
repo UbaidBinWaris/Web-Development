a01a22d27a1c1833f2816cacb196a5c4745d4fab
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-09-04 22:32:41 +0500

    thora thora bas kar raha ho

--- Backend/folder_fs/main.js
+++ Backend/folder_fs/main.js
@@ -1,5 +1,19 @@
-import fs from "fs"
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
 
-let files = await fs.readdir("C:\\Users\\ADMIN\\OneDrive\\Documents\Web development\\Web-Development\\Backend\\folder_fs");
+let basepath =
+  "C:\\Users\\ADMIN\\OneDrive\\Documents\\Web development\\Web-Development\\Backend\\folder_fs";
 
-console.log(files);
+let files = await fs.readdir(basepath);
+
+for (const items of files) {
+  let ext = items.split(".")[items.split(".").length - 1];
+  if (ext != "js" && ext != "json" && ext.split(".").length > 1) {
+    if (fsn.existsSync(ext)) {
+      fs.rename(path.join(basepath, items), path.join(basepath, ext, items));
+    } else {
+      fs.mkdir(ext);
+    }
+  }
+}
