41f56ec04ad9aec6042d6933c1b8fdc7310e93c5
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-09-06 20:20:23 +0500

    ok ho gia ha

--- Backend/dummy_data_db/main.mongodb.js
+++ Backend/dummy_data_db/main.mongodb.js
@@ -13,18 +13,29 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { foo: "FOO" });
 });
+const get_random = (arr) =>{
+  let rno = Math.floor(Math.random() * (arr.length - 1))
+  return arr[rno];
+}
 
 app.get("/genetate", async (req, res) => {
 
+  await Employee.deleteMany({}); // Clear all records before generating new ones
+
+  let random_names = ["Haider" , "Afaq" ,"Salman" , "Jehabzeb"];
+  let random_languages = ["Python", "JavaScript", "Java", "C++"];
+  let random_salary = [10,20,25,4,21];
+  let random_cities = ["Islamabad" , "Karachi" , "lahore" , "Quetta"];
+
   for (let index = 0; index < 10; index++) {
     let e = await Employee.create({
-      name: "Ubaid",
-      salary: 200010,
-      language: "String",
-      city: "Islamabad",
-      isManager: "true",
+      name: get_random(random_names),
+      salary: get_random(random_salary),
+      language: get_random(random_languages),
+      city: get_random(random_cities),
+      isManager: (Math.random() > 0.5 )?true:false
     });
-    // console.log(e);
+    console.log(e);
     await e.save();
   }
 
