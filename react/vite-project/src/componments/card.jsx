62de5c26dc837d22419a272cf874904dfda1638f
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-09-09 23:36:10 +0500

    card ban gia

--- react/vite-project/src/componments/card.jsx
+++ react/vite-project/src/componments/card.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect , useState } from "react";
-
+import "./card.css";
 const Card = () => {
     const [card , setcard] = useState([])
 
@@ -14,16 +14,19 @@ const Card = () => {
     },[])
     return (
         <div className="container">
-            card.map((card) =>{
-                return(
-                    <div key = {card.id}className="card">
-                        <h3>{card.title}</h3>
-                        <p>{card.body}</p>
-                    </div>
-                )
-            })
+          {card.map((card) => {
+            return (
+              <div key={card.id} className="d">
+                <h2>{card.title}</h2>
+                <h3>{card.id}</h3>
+                <h3>{card.userId}</h3>
+                <p>{card.body}</p>
+              </div>
+            );
+          })}
         </div>
-    );
+      );
+      
   };
   
   export default Card;
