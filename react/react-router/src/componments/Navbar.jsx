a3ad32e6f6129295b802538048e4084a211f373d
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-09-16 19:33:34 +0500

    react router

---
+++ react/react-router/src/componments/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navb">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
