ac51bb236829bfe8887905df1015e74fdf5ecce7
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-09-20 13:02:09 +0500

    next server

--- Nextjs/my-app/app/page.js
+++ Nextjs/my-app/app/page.js
@@ -1,7 +1,20 @@
+"use client"
 import Image from "next/image";
+import { POST } from "./api/app/route";
 
 export default function Home() {
+  const Handle = async () => {
+    let a = await fetch(/api/add/route.js , {method:POST , headers:{"Content-Type" : "application/json"} , body:JSON.stringify(data),);
+    let b = await a.json();
+    console.log(b); 
+  };
+
   return (
-    <div>I am Home page</div>
+    <div className="flex-col">
+      <span className="text-xl">I am Home page</span>
+      <button onClick={Handle} className="border p-2 border-red-600">
+        Click me
+      </button>
+    </div>
   );
 }
