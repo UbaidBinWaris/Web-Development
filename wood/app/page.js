639678df6963a720f4aefe9f37b39239f2567196
Ubaid-Bin-Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2024-10-12 23:36:50 +0500

    wood wali website

---
+++ wood/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import header from "@/components/header";
+
+export default function Home() {
+  return (
+    <div className="book">
+      hii
+      <header />
+    </div>
+  );
+}
---
+++ wood/components/header.jsx
@@ -0,0 +1,13 @@
+// 'use client';
+
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='bg-red-400'>
+      Hi i am header
+    </div>
+  )
+}
+
+export default header
