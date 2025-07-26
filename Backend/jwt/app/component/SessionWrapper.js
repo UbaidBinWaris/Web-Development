f2f0a8a961a37c1e7b6e939247fd83fa84feb1d4
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-07-26 18:22:56 +0500

    learning next auth

---
+++ Backend/jwt/app/component/SessionWrapper.js
@@ -0,0 +1,12 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+import React from 'react'
+
+export const SessionWrapper = ({ children }) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
