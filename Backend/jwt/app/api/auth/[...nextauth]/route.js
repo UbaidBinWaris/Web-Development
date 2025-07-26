b8815f4bcc578da54bbf139e4ec651a48c6c2a7d
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-07-26 20:09:58 +0500

    idk

--- Backend/jwt/app/api/auth/[...nextauth]/route.js
+++ Backend/jwt/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
+import { supabase } from "@/lib/supabase";
 
 const handler = NextAuth({
   providers: [
@@ -8,7 +9,29 @@ const handler = NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-  ]
+  ],
+//   callbacks: {
+//     async signIn({ user, account }) {
+//       // Save to Supabase on first sign in
+//       try {
+//         const { data, error } = await supabase
+//           .from('users')
+//           .upsert({
+//             github_id: account.providerAccountId,
+//             name: user.name,
+//             email: user.email,
+//           }, {
+//             onConflict: 'github_id', // avoid duplicates
+//           });
+
+//         if (error) throw error;
+//         return true;
+//       } catch (err) {
+//         console.error("Supabase insert error:", err);
+//         return false;
+//       }
+//     },
+//   },
 })
 
 export { handler as GET, handler as POST }
